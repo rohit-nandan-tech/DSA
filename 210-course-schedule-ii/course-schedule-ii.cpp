327c0c6cd8c64d2839059af669ebc8142f285885
rohit-nandan-tech <irohitemail@gmail.com>
2025-09-17 21:54:23 +0530

    Time: 4 ms (56.34%) | Memory: 18.9 MB (26.58%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int num, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(num);
+        vector<int>indeg(num, 0);
+        for(auto n: pre){
+            adj[n[1]].push_back(n[0]);
+            indeg[n[0]]++;
+        }
+
+        queue<int>q;
+        for(int i = 0 ; i<num;i++){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+
+
+        int cnt = 0;
+
+        vector<int>ans;
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+
+            for(auto n: adj[f]){
+                indeg[n]--;
+                if(indeg[n]==0){
+                    q.push(n);
+                }
+            }
+            ans.push_back(f);
+        }
+
+        if(ans.size() != num)
+            return {};
+
+        return ans;
+
+    }
+};
