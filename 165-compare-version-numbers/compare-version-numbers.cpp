c298a78ca1d3d837f4c0ed4ca2a4af7e6ab87326
rohit-nandan-tech <irohitemail@gmail.com>
2025-09-24 01:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (69.57%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        vector<string>ver1(501, "0");
-        vector<string>ver2(501,"0");
-        int m =0;
-        string s = "";
-        for(int i = 0 ; i<=version1.size();i++){
-            if(i == version1.size()){
-                ver1[m] = s;
-                m++;
-                s= "";
-                continue;
-            }
-            if(version1[i]=='.'){
-                ver1[m] = s;
-                m++;
-                s= "";
-                continue;
-            }
-            s.push_back(version1[i]);
-        }
-        m = 0;
-        s = "";
+        int i = 0, j = 0;
+        int n = version1.size(), m = version2.size();
 
-        for(int i = 0 ; i<=version2.size();i++){
-            if(i == version2.size()){
-                ver2[m] = s;
-                m++;
-                s= "";
-                continue;
-            }
-            if(version2[i]=='.'){
-                ver2[m] = s;
-                m++;
-                s= "";
-                continue;
-            }
-            s.push_back(version2[i]);
-        }
+        while(i < n || j < m){
+            long num1 = 0, num2 = 0;
 
-
-        for(int i = 0; i<=500;i++){
-            if(stoi(ver1[i]) >  stoi(ver2[i])){
-                return 1 ;
+            // version1 se ek block number nikal lo
+            while(i < n && version1[i] != '.'){
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
             }
-            else if(stoi(ver1[i]) <  stoi(ver2[i]))
-                return -1;
+            i++; // dot skip karo
+
+            // version2 se ek block number nikal lo
+            while(j < m && version2[j] != '.'){
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+            j++; // dot skip karo
+
+            // compare karo
+            if(num1 > num2) return 1;
+            if(num1 < num2) return -1;
         }
 
         return 0;
