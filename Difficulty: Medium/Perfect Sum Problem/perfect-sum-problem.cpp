65b9ae18ab9e9cc03223289ee2f8dea77c311929
rohit-nandan-tech <irohitemail@gmail.com>
2026-01-08 22:33:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,19 +1,25 @@
 class Solution {
-  public:
-    int perfectSum(vector<int>& arr, int target) {
-        int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(target+1,0));
-       
-        for(int i=0;i<n;i++)dp[i][0]=1;
-        
-        if(target>=arr[0])dp[0][arr[0]]+=1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=target;j++){
-                int p=(arr[i]<=j)?dp[i-1][j-arr[i]]:0;
-                int np=dp[i-1][j];
-                dp[i][j]=p+np;
-            }
+public:
+    int solve(int idx, int tar, vector<vector<int>>& dp, vector<int>& arr) {
+        if (idx == 0) {
+            if (tar == 0 && arr[0] == 0) return 2;
+            if (tar == 0 || arr[0] == tar) return 1;
+            return 0;
         }
-        return dp[n-1][target];
+
+        if (dp[idx][tar] != -1) return dp[idx][tar];
+
+        int notTake = solve(idx - 1, tar, dp, arr);
+        int take = 0;
+        if (arr[idx] <= tar)
+            take = solve(idx - 1, tar - arr[idx], dp, arr);
+
+        return dp[idx][tar] = take + notTake;
+    }
+
+    int perfectSum(vector<int>& arr, int tar) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
+        return solve(n - 1, tar, dp, arr);
     }
 };
